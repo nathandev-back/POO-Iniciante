cf8886439662cbde52fd9cb6f5cb576f5fff2964
Novak <nathandev1227@gmail.com>
2024-11-18 23:07:05 -0300

    first commit

---
+++ src/exibir.java
@@ -0,0 +1,18 @@
+public class exibir {
+    public static void Informacoes(Produto produto) {
+        
+        System.out.print("""
+                ========================
+                Informações do produto
+                
+                Produto = %s
+                Preço = %d
+                Quantidade = %d
+                
+                ========================
+                
+                
+                """.formatted(produto.getName(),produto.getPreco(), produto.getQuantia()));
+    }
+    
+}
